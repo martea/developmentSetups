41c6551b387bfe55aa35609f2aa509b1d2fa3a50
Mårten Andersson <maarten.andersson@gmail.com>
2018-03-05 16:44:39 +0100

    updated with less and scss

--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -1,27 +1,48 @@
 const path = require("path");
 
 module.exports = {
+    entry: {
+        "site": "./src/js/index.js",
+        "style": "./src/scss/site.scss",
+    },
     resolve: {
         alias: {
-            material: path.resolve(
-                __dirname,
-                "node_modules/material-design-lite/material.min.js"
-            ),
-            styles: path.resolve(__dirname, "Src/scss/styles.scss"),
-            $utils: path.resolve(__dirname, "./src/js/utils/"),
-            $services: path.resolve(__dirname, "./src/module/"),
-            $modules: path.resolve(__dirname, "./src/js/modules/"),
-            $pages: path.resolve(__dirname, "./src/js/pages/"),
-            $components: path.resolve(__dirname, "./src/js/components")
+            $services: path.resolve(__dirname, "./src/js/module/")
         }
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+        rules: [
+            //Stylesheets
+            {
+                test: /\.less$/,
+                use: [
+                    // creates style nodes from JS strings
+                    { loader: "style-loader" },
+                    // translates CSS into CommonJS
+                    { loader: "css-loader" },
+                    // compiles Less to CSS
+                    { loader: "less-loader" }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    // creates style nodes from JS strings
+                    { loader: "style-loader" },
+                    // translates CSS into CommonJS
+                    { loader: "css-loader" },
+                    // compiles Sass to CSS
+                    { loader: "sass-loader" }
+                ]
+            },
+            //Javascript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
-        }]
+        ]
     }
 };
