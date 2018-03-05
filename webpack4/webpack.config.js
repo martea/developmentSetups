e5f42b2f82bbe1a2f4640a3913c111ec58bbd3a4
Mårten Andersson <maarten.andersson@gmail.com>
2018-03-05 17:55:26 +0100

    updates

--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -14,16 +14,13 @@ module.exports = {
         }
     },
     module: {
-        rules: [
-            //Stylesheets
-            {
+        rules: [{
                 test: /\.less$/,
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
                     use: ["css-loader", "less-loader"]
                 })
             },
-            //Javascript
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
